6b1aea487b6d0c370b266a6881dbbfd5f03f61f1
vishallmaurya <vishallmaurya210@gmail.com>
2024-09-08 19:07:01 +0530

    added

---
+++ try.py
@@ -0,0 +1,34 @@
+# stopwords = ["is", "a", "am", "this"]
+data = ['hello', 'this', 'is', 'a', 'link', 'im', 'hostel']
+
+# ans = []
+# for i in range(len(data)):
+#     if data[i] in stopwords:
+#         continue
+#     ans.append(data[i])
+
+# print(data)
+# print(ans)
+
+Stemming --> sneezing --> sneez
+lemmatizing --> sneezing --> sneeze
+
+swimming
+
+swim
+
+Word Embeddings
+
+[this is a apple
+this is banana
+this is a rat]
+
+
+apple-> 0.343434
+banan-> 0.434343
+rat->0.823232
+
+(king, queen)
+esteemed personality
+man, woman
+
